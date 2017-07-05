c3c7c9496960a97488ab86482f38795499a92390
Patrick Luboobi <patricklu2010@gmail.com>
2017-07-05 09:36:17 +0300

    Write code for error raising

--- max_min/find_max_min.py
+++ max_min/find_max_min.py
@@ -1,2 +1,18 @@
 def find_max_min(arg_list) -> list:
-    pass
+    """
+    Finds max and minimum number from a list and returns them in list
+    Args:
+        arg_list: A list of integers
+    Raises:
+        ValueError: If non int found in list arg
+    Returns:
+        list: Containing maximum and minimum number
+    """
+
+    # Argument should be a list
+    if not isinstance(arg_list, list):
+        raise TypeError('Argument should be a list')
+
+    # All list items should be numbers
+    elif not all(isinstance(item, int) for item in arg_list):
+        raise ValueError('All list items should be integers')
